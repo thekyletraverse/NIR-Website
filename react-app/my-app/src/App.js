9510c4b40a09acc2130f0a0b43f5454da8f912dc
Kyle Traverse <ktraver2@lion.lmu.edu>
2022-04-20 23:36:57 -0700

    added sound button to honme page

--- react-app/my-app/src/App.js
+++ react-app/my-app/src/App.js
@@ -4,24 +4,59 @@ import monst from './NI Monster.png'
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./Navigation.js";
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import useSound from 'use-sound';
+import { Button, Container} from 'react-bootstrap'
+import music from './JAZZ VIBES.mp3';
+import React, { useState } from 'react';
+
 
 function App() {
-  return (
 
+  const [play, { stop }] = useSound(
+      music,
+      { volume: 0.5 }
+    );
+
+    const [isHovering, setIsHovering] = React.useState(
+      false
+    );
+
+
+
+  return (
     <div className="App">
+    <div>
     <Navbar />
       <header className="App-header">
         <img src={monst} className="App-logo" alt="logo" />
         <a
-          className="App-link"
+          className="bg-image"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-
         </a>
       </header>
+      <Button
+          onMouseEnter={() => {
+            setIsHovering(true);
+            play();
+          }}
+          onMouseLeave={() => {
+            setIsHovering(false);
+            stop();
+          }}
+        >
+        <span role="img" aria-label="trumpet">
+     🎺
+   </span>
+        </Button>
+  
     </div>
+
+    </div>
+
+
   );
 }
 
