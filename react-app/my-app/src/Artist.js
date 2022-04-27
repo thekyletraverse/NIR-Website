fc9c8530b5cc0e0f03aec379294bbc83ae8f158b
Kyle Traverse <ktraver2@lion.lmu.edu>
2022-04-27 00:14:59 -0700

    artists and music pages

--- react-app/my-app/src/Artist.js
+++ react-app/my-app/src/Artist.js
@@ -1,23 +1,129 @@
 import React from "react";
+import dog from './ArtistIMG/IMG_6958.jpeg'
+import goop from './ArtistIMG/IMG_1453.JPEG'
+import kev from './ArtistIMG/GKShadow Edit_1.jpeg'
+import tim from './ArtistIMG/BDC4FF5F-2A7C-4825-ACD4-503488E7A383.JPG'
+import tom from './ArtistIMG/IMG_5750.JPG'
+import hunt from './ArtistIMG/Screen Shot 2022-04-26 at 8.24.42 PM.png'
+import peer from './ArtistIMG/Screen Shot 2022-04-26 at 8.33.57 PM.png'
+import pig from './ArtistIMG/Screen Shot 2022-04-26 at 8.33.13 PM.png'
+import kyle from './ArtistIMG/Screen Shot 2022-04-26 at 8.49.24 PM.png'
+import just from './ArtistIMG/Screen Shot 2022-04-26 at 8.46.36 PM.png'
+import jay from './ArtistIMG/Screen Shot 2022-04-26 at 8.47.46 PM.png'
+import boat from './ArtistIMG/Screen Shot 2022-04-26 at 8.56.49 PM.png'
+import Footer from './footer'
+import { Button, Container} from 'react-bootstrap'
+
+import { FaInstagram, FaTwitter, FaArrowRight} from 'react-icons/fa';
 
 export default function Artist() {
   return (
+    <div>
     <div class="art">
        <h1>ARTISTS</h1>
        <div class="container">
          <div class="row">
            <div class="col">
-             <h1>Coffee Dog</h1>
+             <h1>COFFEE DOG</h1>
+             <img src={dog} width="300" height="120"/>
+             <Button><FaInstagram/></Button>
+             <Button><FaTwitter/></Button>
            </div>
            <div class="col">
-             <h1>Goopily</h1>
+             <h1>GOOPILY</h1>
+             <img src={goop} width="300" height="120"/>
+             <Button><FaInstagram/></Button>
+             <Button><FaTwitter/></Button>
            </div>
            <div class="col">
-             <h1>Group Kevin</h1>
+             <h1>GROUP KEVIN</h1>
+             <img src={kev} width="300" height="120"/>
+             <Button><FaInstagram/></Button>
+             <Button><FaTwitter/></Button>
+           </div>
+           <div class="col">
+             <h1>HUNTER LIVSEY</h1>
+             <img src={hunt} width="300" height="120"/>
+             <h1>Official Site</h1>
+             <Button><FaInstagram/></Button>
+             <Button><FaTwitter/></Button>
+           </div>
+         </div>
+         <div class="row">
+           <div class="col">
+             <h1>JAY OLIVE</h1>
+             <img src={jay} width="300" height="120"/>
+             <Button><FaInstagram/></Button>
+             <Button><FaTwitter/></Button>
+           </div>
+           <div class="col">
+             <h1>JUSTIN SCHWARTZ</h1>
+             <img src={just} width="300" height="120"/>
+             <Button><FaInstagram/></Button>
+             <Button><FaTwitter/></Button>
+           </div>
+           <div class="col">
+             <h1>KYLE TRAVERSE</h1>
+             <img src={kyle} width="300" height="120"/>
+             <Button><FaInstagram/></Button>
+             <Button><FaTwitter/></Button>
+           </div>
+           <div class="col">
+             <h1>LICENSED BOAT OPERATORS</h1>
+             <img src={boat} width="300" height="120"/>
+             <Button><FaInstagram/></Button>
+             <Button><FaTwitter/></Button>
+           </div>
+         </div>
+         <div class="row">
+           <div class="col">
+             <h1>PEER PRESSURE</h1>
+             <img src={peer} width="300" height="120"/>
+             <Button><FaInstagram/></Button>
+             <Button><FaTwitter/></Button>
+           </div>
+           <div class="col">
+             <h1>TIM FOIL</h1>
+             <img src={tim} width="300" height="120"/>
+             <h1>Official Site</h1>
+             <Button><FaInstagram/></Button>
+             <Button><FaTwitter/></Button>
+           </div>
+           <div class="col">
+             <h1>TOM808</h1>
+             <img src={tom} width="300" height="120"/>
+             <Button><FaInstagram/></Button>
+             <Button><FaTwitter/></Button>
+           </div>
+           <div class="col">
+             <h1>THE PIG, VERBATIM</h1>
+             <img src={pig} width="300" height="120"/>
+             <Button><FaInstagram/></Button>
+             <Button><FaTwitter/></Button>
+           </div>
+         </div>
+         <div class="row">
+           <div class="col">
+             <h1></h1>
+
+           </div>
+           <div class="col">
+             <h1></h1>
+
+           </div>
+           <div class="col">
+             <h1></h1>
+
+           </div>
+           <div class="col">
+             <h1></h1>
+
            </div>
          </div>
        </div>
-    </div>
 
+    </div>
+<Footer/>
+</div>
   );
 }
--- react-app/my-app/src/Music.js
+++ react-app/my-app/src/Music.js
@@ -1,11 +1,217 @@
 import React from "react";
 import Footer from './footer'
+import bean from './Covers/Screen Shot 2022-04-26 at 9.39.43 PM.png'
+import bum from './Covers/rsz_bum_real_cover.jpg'
+import metal from './Covers/Screen Shot 2022-04-26 at 9.40.01 PM.png'
+import text from './Covers/Screen Shot 2022-04-26 at 9.46.56 PM.png'
+import berg from './Covers/Screen Shot 2022-04-26 at 9.40.41 PM.png'
+import nin from './Covers/Screen Shot 2022-04-26 at 9.41.08 PM.png'
+import tin from './Covers/Screen Shot 2022-04-26 at 9.47.10 PM.png'
+import verb from './Covers/Screen Shot 2022-04-26 at 9.42.39 PM.png'
+import dogg from './Covers/Screen Shot 2022-04-26 at 9.42.39 PM.png'
+import visc from './Covers/Screen Shot 2022-04-26 at 9.41.38 PM.png'
+import hook from './Covers/Screen Shot 2022-04-26 at 9.41.54 PM.png'
+import guyz from './Covers/Screen Shot 2022-04-26 at 10.06.25 PM.png'
+import pop from './Covers/Screen Shot 2022-04-26 at 9.47.24 PM.png'
+import eight from './Covers/Screen Shot 2022-04-26 at 9.49.14 PM.png'
+import mead from './Covers/Screen Shot 2022-04-26 at 9.43.44 PM.png'
+import spare from './Covers/Screen Shot 2022-04-26 at 9.46.09 PM.png'
+import beaut from './Covers/Screen Shot 2022-04-26 at 9.45.49 PM.png'
+import cheese from './Covers/Screen Shot 2022-04-26 at 9.48.13 PM.png'
+import vib2 from './Covers/Screen Shot 2022-04-26 at 9.44.57 PM.png'
+import fran from './Covers/Screen Shot 2022-04-26 at 9.46.32 PM.png'
+import harl from './Covers/Screen Shot 2022-04-26 at 9.44.02 PM.png'
+import who from './Covers/Screen Shot 2022-04-26 at 10.06.47 PM.png'
+import grass from './Covers/Screen Shot 2022-04-26 at 9.44.39 PM.png'
+import sprite from './Covers/Screen Shot 2022-04-26 at 10.07.06 PM.png'
+import cabb from './Covers/Screen Shot 2022-04-26 at 9.45.18 PM.png'
+import lett from './Covers/Screen Shot 2022-04-26 at 9.47.57 PM.png'
+import dear from './Covers/Screen Shot 2022-04-26 at 9.48.55 PM.png'
+import withy from './Covers/Screen Shot 2022-04-26 at 9.50.27 PM.png'
+import jams from './Covers/Screen Shot 2022-04-26 at 9.43.19 PM.png'
+import casa from './Covers/Screen Shot 2022-04-26 at 9.48.40 PM.png'
+import vib from './Covers/Screen Shot 2022-04-26 at 9.44.25 PM.png'
+import cac from './Covers/Screen Shot 2022-04-26 at 9.43.01 PM.png'
+import mayo from './Covers/Screen Shot 2022-04-26 at 9.47.45 PM.png'
 
 export default function Music() {
   return (
     <div>
     <div class="music">
       <h1>MUSIC</h1>
+      <div class="container">
+        <div class="row">
+          <div class="col">
+
+            <img src={bean} width="300" height="120"/>
+
+          </div>
+          <div class="col">
+
+            <img src={bum} width="300" height="120"/>
+
+          </div>
+          <div class="col">
+
+            <img src={metal} width="300" height="120"/>
+
+          </div>
+          <div class="col">
+
+            <img src={text} width="300" height="120"/>
+
+          </div>
+        </div>
+        <div class="row">
+          <div class="col">
+
+            <img src={berg} width="300" height="120"/>
+
+          </div>
+          <div class="col">
+
+            <img src={nin} width="300" height="120"/>
+
+          </div>
+          <div class="col">
+
+            <img src={tin} width="300" height="120"/>
+
+          </div>
+          <div class="col">
+
+            <img src={verb} width="300" height="120"/>
+
+          </div>
+        </div>
+        <div class="row">
+          <div class="col">
+
+            <img src={dogg} width="300" height="120"/>
+
+          </div>
+          <div class="col">
+            <img src={visc} width="300" height="120"/>
+
+          </div>
+          <div class="col">
+            <img src={hook} width="300" height="120"/>
+          </div>
+          <div class="col">
+            <img src={guyz} width="300" height="120"/>
+
+          </div>
+        </div>
+        <div class="row">
+          <div class="col">
+          <img src={pop} width="300" height="120"/>
+
+          </div>
+          <div class="col">
+            <img src={eight} width="300" height="120"/>
+
+          </div>
+          <div class="col">
+            <img src={mead} width="300" height="120"/>
+
+          </div>
+          <div class="col">
+            <img src={spare} width="300" height="120"/>
+
+          </div>
+        </div>
+        <div class="row">
+          <div class="col">
+          <img src={beaut} width="300" height="120"/>
+
+          </div>
+          <div class="col">
+            <img src={cheese} width="300" height="120"/>
+
+          </div>
+          <div class="col">
+            <img src={vib2} width="300" height="120"/>
+
+          </div>
+          <div class="col">
+            <img src={fran} width="300" height="120"/>
+
+          </div>
+        </div>
+        <div class="row">
+          <div class="col">
+          <img src={harl} width="300" height="120"/>
+
+          </div>
+          <div class="col">
+            <img src={who} width="300" height="120"/>
+
+          </div>
+          <div class="col">
+            <img src={grass} width="300" height="120"/>
+
+          </div>
+          <div class="col">
+            <img src={sprite} width="300" height="120"/>
+
+          </div>
+        </div>
+        <div class="row">
+          <div class="col">
+          <img src={cabb} width="300" height="120"/>
+
+          </div>
+          <div class="col">
+            <img src={lett} width="300" height="120"/>
+
+          </div>
+          <div class="col">
+            <img src={dear} width="300" height="120"/>
+
+          </div>
+          <div class="col">
+            <img src={withy} width="300" height="120"/>
+
+          </div>
+        </div>
+        <div class="row">
+          <div class="col">
+          <img src={jams} width="300" height="120"/>
+
+          </div>
+          <div class="col">
+            <img src={casa} width="300" height="120"/>
+
+          </div>
+          <div class="col">
+            <img src={vib} width="300" height="120"/>
+
+          </div>
+          <div class="col">
+            <img src={cac} width="300" height="120"/>
+
+          </div>
+        </div>
+        <div class="row">
+          <div class="col">
+          <img src={mayo} width="300" height="120"/>
+
+          </div>
+          <div class="col">
+
+
+          </div>
+          <div class="col">
+
+
+          </div>
+          <div class="col">
+    
+
+          </div>
+        </div>
+      </div>
+
    </div>
       <div>
         <Footer/>
