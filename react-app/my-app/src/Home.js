56c2265b37ebd9e649d523585215cf2dc606c14d
Kyle Traverse <ktraver2@lion.lmu.edu>
2022-04-28 09:19:11 -0700

    home page tweaks

--- react-app/my-app/src/Home.js
+++ react-app/my-app/src/Home.js
@@ -29,8 +29,6 @@ function Home() {
       <div className="App-header">
         <div>
           <div style={{ height: 165, marginTop: 6 }}class="col">
-          <img src={notta} width="250"
-            height="200" className="App-logo" alt="logo" />
           <h1 class="h1" style={{fontSize: "1rem"}}>Hover!</h1>
           <Button variant= "dark" size="xl"
               onMouseEnter={() => {
@@ -46,12 +44,14 @@ function Home() {
 
           </span>
             </Button>
+            <img src={notta} width="250"
+              height="200" className="App-logo" alt="logo" />
           </div>
           <div class="col">
              <FaCloud class="Cloud-logo"size="lg"/>
-             <FaCloud class="Cloud-logo3" size="lg"/>
              <img src={monst} width="300"
                height="120" className="image-style6" alt="logo" />
+             <FaCloud class="Cloud-logo3" size="lg"/>
                <img src={look} width="300"
                  height="120" className="image-style6" alt="logo" />
           </div>
