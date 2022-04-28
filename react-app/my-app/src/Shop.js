5cb65de1dc1200cf0fc1a0403a53632684596552
Kyle Traverse <ktraver2@lion.lmu.edu>
2022-04-28 09:14:41 -0700

    shop page clean up

--- react-app/my-app/src/Shop.js
+++ react-app/my-app/src/Shop.js
@@ -31,23 +31,23 @@ export default function Shop() {
              <i class="button-style"><FaShopify/></i>
            </a>
        </div>
-       <div style={{padding:"1px"}} class="row justify-content-md-center">
+       <div style={{padding:"10px"}} class="row justify-content-md-center">
          <div style={{padding:"5px", textAlign:"center"}} class="col">
            <img class="image-style4 justify-content-md-center"src={tag} width="150" height="20"/>
            <text style={{padding:"3px"}} class="h2">*Featuring Custom Tag!*</text>
          </div>
        </div>
 
-       <div style={{padding:"1px"}} class="row justify-content-md-center">
-         <div style={{padding:"5px", textAlign:"center"}} class="col">
+       <div style={{padding:"50px"}} class="row justify-content-md-center">
+         <div style={{padding:"5px", textAlign:"center"}} class="col justify-content-md-center">
            <img class="image-style4 justify-content-md-center"src={tap} width="350" height="120"/>
-           <text style={{padding:"3px"}} class="h2">NIR Tapestry</text>
-           <text style={{padding:"3px"}} class="h2">Coming Soon</text>
+           <text class="button-style h2">NIR Tapestry</text>
+           <text class="button-style h2">Coming Soon</text>
            <a href="">
              <i class="button-style"><FaShopify/></i>
            </a>
          </div>
-         <div style={{padding:"5px", textAlign:"center"}} class="col">
+         <div style={{padding:"50px", textAlign:"center"}} class="col">
            <img class="image-style4 justify-content-md-center"src={stick} width="350" height="120"/>
            <text style={{padding:"3px"}} class="h2">NIR Sticker</text>
            <text style={{padding:"3px"}} class="h2">Coming Soon</text>
