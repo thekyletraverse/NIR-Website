6340a00a58c2bd3a1a9de192e825f4484b0795ea
Kyle Traverse <ktraver2@lion.lmu.edu>
2022-04-29 23:07:00 -0700

    added links to header

--- react-app/my-app/src/Navigation.js
+++ react-app/my-app/src/Navigation.js
@@ -1,8 +1,9 @@
 import React from "react";
-import { Button, Navbar, Container} from 'react-bootstrap'
+import { Button, Navbar, Container, Form, FormControl} from 'react-bootstrap'
 import { Link, Router, Route } from "react-router-dom";
 import Nav from "react-bootstrap/Nav";
 import { LinkContainer } from "react-router-bootstrap";
+import { FaInstagram, FaTwitter, FaYoutube} from 'react-icons/fa';
 
 
 const Navigation = () => {
@@ -35,6 +36,20 @@ const Navigation = () => {
         <Nav.Link>Contact</Nav.Link>
       </LinkContainer>
     </Nav>
+    <Nav style={{padding:"75px"}}>
+    
+          </Nav>
+    <Nav style={{padding:"75px"}}>
+    <a href="https://www.instagram.com/nothingiconicrecords/?utm_source=ig_embed&amp;utm_campaign=loading">
+    <i class="button-style2 fa-2x"><FaInstagram/></i>
+    </a>
+    <a href="https://twitter.com/IconicNothing?s=20&t=QV86wjoDvNA5AsHq9WAatA">
+     <i class="button-style2 fa-2x"><FaTwitter/></i>
+    </a>
+    <a href="https://twitter.com/Jp__Shannon?s=20&t=GBPCVAbq8lpdNN0hEMEIrw">
+     <i class="button-style2 fa-2x"><FaYoutube/></i>
+    </a>
+          </Nav>
     </Navbar.Collapse>
   </Navbar>
 
