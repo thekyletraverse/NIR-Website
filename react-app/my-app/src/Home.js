0d70c9860dcff4273a41d5a7684dba64c1135a8e
Kyle Traverse <ktraver2@lion.lmu.edu>
2022-04-30 12:55:09 -0700

    added music snippets to home page

--- react-app/my-app/src/Home.js
+++ react-app/my-app/src/Home.js
@@ -8,20 +8,33 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Routes, Route, Switch } from "react-router-dom";
 import useSound from 'use-sound';
 import { Button, Container} from 'react-bootstrap'
-import audio from './react-app_my-app_src_JAZZ VIBES.mp3';
+import audio1 from './AudioFiles/FADE AWAY SNIPPET.wav';
+import audio2 from './AudioFiles/HUEW SNIPPET.wav';
+import audio3 from './AudioFiles/PAINT PICTURES SNIPPET.wav';
 import React, { useState } from 'react';
 import { FaPlay, FaCloud} from 'react-icons/fa';
 import Footer from './footer'
 
 
 function Home() {
-  const audio = new Audio(
-    "./react-app_my-app_src_JAZZ VIBES.mp3"
+
+  const [play] = useSound(
+    audio1,
+    { volume: 0.5 }
+  );
+  const [play2, { stop2 }] = useSound(
+    audio2,
+    { volume: 0.5 }
+  );
+  const [play3, { stop3 }] = useSound(
+    audio3,
+    { volume: 0.5 }
+  );
+  const [isHovering, setIsHovering] = React.useState(
+    false
   );
 
-  const start = () => {
-    audio.play();
-  };
+
   return (
     <div>
       <div className="App-header">
@@ -32,7 +45,11 @@ function Home() {
 
           <div style={{ padding: "20px", height: 80, marginTop: 10 }}class="col">
 
-            <a class="button-style3 fa-2x" onClick={start}><FaPlay/></a>
+            <a class="button-style3 fa-2x" onMouseEnter={() => {
+
+        play();
+      }}
+    ><FaPlay/></a>
           </div>
 
           <div style={{ height: 80, marginTop: 10 }}class="col">
@@ -52,7 +69,7 @@ function Home() {
 
           <div class="col">
           <img src={monst} width="300"
-              height="120" className="image-style6" alt="logo" />
+              height="80" className="image-style6" alt="logo" />
 
              <FaCloud class="Cloud-logo3" size="lg"/>
 
@@ -80,17 +97,30 @@ function Home() {
           <div class="col">
 
             <img src={splat} width="300"
-                  height="120" className="image-style6" alt="logo" />
+                  height="80" className="image-style6" alt="logo" />
 
 
-<i style={{padding: "50px"}}class="button-style fa-2x"><FaPlay/></i>
+                  <a class="button-style3 fa-2x" onMouseEnter={() => {
+
+                  play2();
+                  }}
+                  onMouseLeave={() => {
+                  stop2();
+                  }}><FaPlay/></a>
              <FaCloud class="Cloud-logo" size="sm"/>
           </div>
 
           <div class="col">
 
              <FaCloud class="Cloud-logo "size="lg"/>
-             <i style={{padding:"20px"}}class="button-style fa-2x"><FaPlay/></i>
+             <i style={{padding:"20px"}}class="button-style fa-2x" onMouseEnter={() => {
+         setIsHovering(true);
+         play3();
+       }}
+       onMouseLeave={() => {
+         setIsHovering(false);
+         stop3();
+       }}><FaPlay/></i>
              <FaCloud class="Cloud-logo2" size="lg"/>
 
           </div>
