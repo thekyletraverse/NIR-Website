52e25e515b334a8d822eb92b6adb10c513deaf25
Kyle Traverse <ktraver2@lion.lmu.edu>
2022-04-30 15:26:30 -0700

    added contact form

--- react-app/my-app/src/Contact.js
+++ react-app/my-app/src/Contact.js
@@ -2,6 +2,7 @@ import React from "react";
 import Footer from './footer'
 import { Button, Container} from 'react-bootstrap'
 import open from './IMG_4333.JPG'
+import { ReactMailchimpEmailSignupForm } from 'react-mailchimp-email-signup-form';
 
 
 export default function Contact() {
@@ -9,8 +10,13 @@ export default function Contact() {
     <div>
     <div class="con">
     <h1 style={{padding:"30px"}} class="h1">CONTACT</h1>
-    <h1 style={{padding:"30px"}} class="h2">SIGN UP FOR OUR MAILING LIST!</h1>
-    
+    <ReactMailchimpEmailSignupForm
+       elementId="first-email-signup-form"
+       url="https://gmail.us10.list-manage.com/subscribe/post?u=e25e5c63d90e7b32bf185aac5&amp;id=317777b268" 
+       title="Subscribe to the Newsletter"
+       subtitle="We take privacy seriously and we'll never spam or sell your information."
+     />
+
     </div>
 
 <Footer/>
